4c6f9150c69ada224f61624a96125539d3118a88
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-11-26 02:07:42 +0200

    searchTerm for downloading

--- helpers/fetchEmail.js
+++ helpers/fetchEmail.js
@@ -1,13 +1,14 @@
 var Imap = require("node-imap");
 var fs = require("fs");
-  const moment = require('moment');
+const moment = require("moment");
 const {
   POP3_CLIENT_PORT,
   POP3_CLIENT_HOST,
   POP3_CLIENT_PASSWORD,
   POP3_CLIENT_USERNAME,
 } = process.env;
-
+const searchTerm = "Состояние"; // The word to search for in the subject
+let latestEmailUID = null;
 const fetchEmail = async () => {
   var imap = new Imap({
     user: POP3_CLIENT_USERNAME,
@@ -16,42 +17,55 @@ const fetchEmail = async () => {
     port: POP3_CLIENT_PORT,
     tls: true,
   });
-  const yesterday = moment().subtract(1, "days").toDate();
   function openInbox(cb) {
     imap.openBox("INBOX", true, cb);
   }
-
   imap.once("ready", function () {
     openInbox(function (err, box) {
       if (err) throw err;
 
-      imap.search(["UNSEEN", ["SINCE", yesterday]], function (err, results) {
-        if (err) throw err;
+      imap.search(
+        ["UNSEEN", ["HEADER", "SUBJECT", searchTerm]],
+        function (err, results) {
+          if (err) throw err;
 
-        const fetch = imap.fetch(results, { bodies: "" });
+          // Find the UID of the newest email
+          if (results.length > 0) {
+            latestEmailUID = Math.max(...results);
+          }
 
-        fetch.on("message", function (msg, seqno) {
-          msg.on("body", function (stream, info) {
-            // Use a library like 'mailparser' to parse the email and extract attachments
-            // Here's a basic example using 'mailparser':
-            const simpleParser = require("mailparser").simpleParser;
+          if (latestEmailUID) {
+            const fetch = imap.fetch(latestEmailUID, { bodies: "" });
 
-            simpleParser(stream, (err, mail) => {
-              if (err) throw err;
+            fetch.on("message", function (msg, seqno) {
+              msg.on("body", function (stream, info) {
+                // Use a library like 'mailparser' to parse the email and extract attachments
+                // Here's a basic example using 'mailparser':
+                const simpleParser = require("mailparser").simpleParser;
 
-              mail.attachments.forEach((attachment) => {
-                // Download attachments to a file
-                fs.writeFileSync(attachment.filename, attachment.content);
-                console.log(`Downloaded attachment: ${attachment.filename}`);
+                simpleParser(stream, (err, mail) => {
+                  if (err) throw err;
+
+                  mail.attachments.forEach((attachment) => {
+                    // Download attachments to a file
+                    fs.writeFileSync(attachment.filename, attachment.content);
+                    console.log(
+                      `Downloaded attachment: ${attachment.filename}`,
+                    );
+                  });
+                });
               });
             });
-          });
-        });
 
-        fetch.on("end", function () {
-          imap.end();
-        });
-      });
+            fetch.on("end", function () {
+              imap.end();
+            });
+          } else {
+            console.log("No unread emails with the specified subject found.");
+            imap.end();
+          }
+        },
+      );
     });
   });
 
@@ -64,7 +78,6 @@ const fetchEmail = async () => {
   });
 
   imap.connect();
-
 };
 fetchEmail();
 module.exports = fetchEmail;
