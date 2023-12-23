315edb53262af95403cd68e785f03b86b8110cb9
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-12-23 17:21:01 +0200

    scrollPage

--- helpers/scrapeGoogleSearchResults.js
+++ helpers/scrapeGoogleSearchResults.js
@@ -7,6 +7,20 @@ async function scrapeGoogleSearchResults(query) {
   // Navigate to Google search results page
   await page.goto(`https://www.google.com/search?q=${query}`);
 
+  // Function to scroll the page
+  const scrollPage = async () => {
+    await page.evaluate(() => {
+      window.scrollBy(0, window.innerHeight);
+    });
+    // Wait for a short time to allow the next set of results to load
+    await page.waitForTimeout(1000);
+  };
+
+  // Scroll a few times to load more results (adjust the loop count based on your needs)
+  for (let i = 0; i < 3; i++) {
+    await scrollPage();
+  }
+
   await page.waitForSelector(".fG8Fp.uo4vr"); // ensure at least first result awaited
   // Extract prices from the search results
   const prices = await page.evaluate(() => {
@@ -16,10 +30,10 @@ async function scrapeGoogleSearchResults(query) {
         // 'a[jsname="UWckNb"]',
         ".VuuXrf", // main name
         // ".qLRx3b.tjvcx.GvPZzd.cHaqb", // full path
-      ).textContent;
+      )?.textContent;
       const priceElement = element.querySelector(".fG8Fp.uo4vr");
       const price = priceElement
-        ? priceElement.textContent
+        ? priceElement?.textContent
         : "Price not available";
       return { siteAddress, price };
     });
